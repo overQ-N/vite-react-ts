188aa863d4927d5a95966d1521e235873070395d
haungzn <huangzn@lenxdt.com>
2021-06-21 17:33:30 +0800

    清除crlf差异

--- src/pages/hybird/index.tsx
+++ src/pages/hybird/index.tsx
@@ -1,14 +1,42 @@
 import React, { useEffect } from "react";
-
+import {
+  makeAutoObservable,
+  makeObservable,
+  computed,
+  observable,
+  action,
+  flow,
+} from "mobx";
+import { observer } from "mobx-react";
+import { Button } from "antd";
+class Timer {
+  count = 0;
+  constructor() {
+    makeObservable(this, {
+      increment: action,
+      reduce: action,
+      count: observable,
+    });
+  }
+  increment() {
+    this.count++;
+  }
+  reduce() {
+    this.count--;
+  }
+}
+const timer = new Timer();
+const Btn = observer(({ timer }) => {
+  return <Button>123456{timer.count}</Button>;
+});
 const Hybird = () => {
-  useEffect(() => {
-    fetch("/upc/sysconfig/getFileServiceUrl")
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(`res`, res);
-      });
-  });
-  return <div>Hybird</div>;
+  return (
+    <div>
+      Hybird
+      <button onClick={() => timer.increment()}>+1</button>
+      <Btn timer={timer}></Btn>
+    </div>
+  );
 };
 
 export default Hybird;
